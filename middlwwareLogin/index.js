3fcffae36c073c9d59c700fd7b9f60c2bec87ffd
Rahul Mula <rahulmula.lm10@gmail.com>
2026-05-28 18:02:48 +0530

    middlware login code

--- middlwwareLogin/index.js
+++ middlwwareLogin/index.js
@@ -1,12 +1,29 @@
 const express = require("express");
 const app = express();
-let port=3000;
+let port=4000;
 
-//dynamic get
-app.get('/welcome/:username',(req,res) =>{
-  const username = req.params.username;
-  const role=req.query.role
-  res.send(`Welcome, ${username}! Your role is ${role}.`);
+// Logging middleware
+app.use((req, res, next) => {
+  console.log(`${req.method} request made to ${req.url}`);
+  next();
+});
+
+//get products
+app.get('/products',(req,res) =>{
+  res.send("Here is the list of all products.");
+})
+//POST products
+app.post('/products',(req,res) =>{
+  res.send("A new product has been added.");
+})
+
+//get categories
+app.get('/categories',(req,res) =>{
+  res.send("Here is the list of all categories.");
+})
+//POST categories
+app.post('/categories',(req,res) =>{
+  res.send("A new category has been created.");
 })
 
 
